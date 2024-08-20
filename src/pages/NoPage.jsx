24f56aeb34908627570b289bba2d9fc1b5e30eae
Umka-dev <design.tvin@gmail.com>
2024-08-20 14:39:38 +0200

    add 404 error page

---
+++ src/pages/NoPage.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
+const NoPage = () => {
+  const { palette } = useTheme();
+  return (
+    <Box
+      maxWidth='xl'
+      display='flex'
+      flexDirection='column'
+      justifyContent='center'
+      alignItems='center'
+      textAlign='center'
+      sx={{
+        my: {
+          xs: '120px',
+          sm: '140px',
+          md: '160px',
+          lg: '180px',
+          xl: '200px',
+        },
+      }}
+    >
+      <Box
+        id='page_404'
+        sx={{
+          position: 'relative',
+          color: palette.common.white,
+          fontWeight: 900,
+          fontSize: { xs: '8em', sm: '12em', md: '16em', lg: '20em' },
+          display: 'block',
+          overflow: 'hidden',
+          width: 'fit-content',
+          height: 'max-content',
+          '&:first-letter': {
+            letterSpacing: { xs: '8vmax', sm: '10vmax', md: '12vmax' },
+          },
+          '&:before': {
+            content: '""',
+            backgroundImage:
+              'url("https://staticdelivery.nexusmods.com/mods/1151/images/528-0-1447526230.png")',
+            position: 'absolute',
+            height: '100%',
+            width: '100%',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain',
+            backgroundPosition: 'center',
+          },
+        }}
+      >
+        44
+      </Box>
+      <Typography variant='body1'>
+        The page you are trying to search has been moved to another universe
+      </Typography>
+    </Box>
+  );
+};
+
+export default NoPage;
