80ed8a00e236fdbbcd3d8706b1a8e068ac7a1770
Umka-dev <design.tvin@gmail.com>
2024-12-07 14:45:37 +0100

    add fetcher test

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,20 +1,58 @@
 import { CHARACTER_STATUS } from './constants';
-import { capitalFirst, removeEmptyKeys } from './utils';
+import { capitalFirst, fetcher, removeEmptyKeys } from './utils';
 
-describe('capitalFirst', () => {
-  test('should return correct string', () => {
-    expect(capitalFirst('asd')).toBe('Asd');
+describe('utils', () => {
+  describe('capitalFirst', () => {
+    test('should return correct string', () => {
+      expect(capitalFirst('asd')).toBe('Asd');
+    });
   });
-});
 
-describe('removeEmptyKeys', () => {
-  test('should remove empty key: value from an object', () => {
-    const expected = { name: 'morty', status: CHARACTER_STATUS.alive };
-    expect(
-      removeEmptyKeys({
-        ...expected,
-        gender: undefined,
-      }),
-    ).toStrictEqual(expected);
+  describe('removeEmptyKeys', () => {
+    test('should remove empty key: value from an object', () => {
+      const expected = { name: 'morty', status: CHARACTER_STATUS.alive };
+      expect(
+        removeEmptyKeys({
+          ...expected,
+          gender: undefined,
+        }),
+      ).toStrictEqual(expected);
+    });
+  });
+
+  describe('fetcher', () => {
+    // Mock the global fetch function
+    global.fetch = jest.fn();
+    const url = 'https://api.example.com/data';
+
+    test('should fetch data and return it as JSON', async () => {
+      // Arrange
+      const mockData = { key: 'value' };
+      const mockResponse = {
+        json: jest.fn().mockResolvedValue(mockData),
+      };
+
+      (global.fetch as jest.Mock).mockResolvedValue(mockResponse);
+
+      // Act
+      const expected = await fetcher<typeof mockData>(url);
+
+      // Assert
+      expect(global.fetch).toHaveBeenCalledTimes(1); // Ensure fetch was called
+      expect(global.fetch).toHaveBeenCalledWith(url); // Ensure fetch was called with the correct URL
+      expect(mockResponse.json).toHaveBeenCalledTimes(1); // Ensure json() was called
+      expect(expected).toEqual(mockData); // Ensure the result matches the mock data
+    });
+    test('should throw an error if fetch fails', async () => {
+      // Arrange
+      const errorMessage = 'Network error';
+      const mockError = new Error(errorMessage);
+      (global.fetch as jest.Mock).mockRejectedValue(mockError);
+
+      // Act&Assert
+      await expect(fetcher(url)).rejects.toThrow(errorMessage);
+      expect(global.fetch).toHaveBeenCalledTimes(1); // Ensure fetch was called
+      expect(global.fetch).toHaveBeenCalledWith(url); // Ensure fetch was called with the correct URL
+    });
   });
 });
